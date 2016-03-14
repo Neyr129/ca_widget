c543647eff1b1daca8d35adc8c8bcfd8002cedbc
DNStaver <dnstaver@gmail.com>
2016-03-14 14:29:55 +0300

    almost finished

--- script.js
+++ script.js
@@ -2,57 +2,85 @@
 
   WidgetDemo = function() {
 
-    this.code        = null;
-    this.currentType = null;
+    this.script_path = "http://test.changeagain.me/demo_experiments/widget";
+
+    this.code_field       = null;
+    this.widget_example   = null;
+    this.activeTypeButton = null;
 
     this.setEventsAndConst = function(){
       self = this;
 
-      $(document).ready(function(){
-        self.currentType =  $('.type-switch>button.current');
-        self.code        =  $('.widget-code>textarea');
-        $('.widget-options>.option>label>input, select.already-changed-quantity').on(
-          "change", function(){
-            self.updateWidget();
-          }
-        );
+      this.code_field       =  $('.widget-code>textarea');
+      this.widget_example   =  $('.widget_example');
+      this.activeTypeButton =  $('.type-switch>button.current');
+      $('.widget-options>.option>label>input, select.already-changed-quantity').on(
+        "change", function(){
+          self.updateWidget();
+        }
+      );
 
-        $('.type-switch>button').on("click", function(){
-          self.switchWidgetType( $(this) );
-        });
+      $('.type-switch>button').on("click", function(){
+        self.switchWidgetType( $(this) );
       });
     };
 
-    this.updateWidget = function() {
-      console.log("updated");
-    }
-
-    this.switchWidgetType = function(type_chosen){
-      var currentType = type_chosen.data("type")
-      this.currentType.removeClass("current");
-      this.currentType = type_chosen;
-      type_chosen.addClass("current");
+    this.switchWidgetType = function(chosen_type){
+      var activeTypeButton = chosen_type.data("type")
+      this.activeTypeButton.removeClass("current");
+      this.activeTypeButton = chosen_type;
+      chosen_type.addClass("current");
       this.updateWidget();
     }
 
     this.updateWidget = function(){
-      this.updateExample();
+      this.updateWidgetExample();
       this.updateCode();
     }
 
-    this.updateExample = function(){
-      consle.log("updated example")
-    };
-
     this.updateCode = function(){
-      code = this.code
+      var code = null;
+      var chosen_type = this.activeTypeButton.data("type");
+      if(chosen_type == "HTML"){
+        this.updateWidgetExample();
+        code = this.getCurrentWidgetUrlFor("html");
+        console.log("code for HTML updated")
+      }else if (chosen_type == "JS") {
+        url  =  this.getCurrentWidgetUrlFor("js");
+        code = "<script type='text/javascript' src='" + url + "</script>";
+        console.log("code for js updated");
+      };
+      this.code_field.text(code);
     };
 
+    this.updateWidgetExample = function(){
+      jQuery.ajax({
+        url:  this.getCurrentWidgetUrlFor("html"),
+        type: "GET",
+        success: function(data) { this.widget_example.html(data) }
+      });
+    };
+
+    this.getCurrentWidgetUrlFor = function(type){
+      url = this.script_path+"."+type+"?" +
+        "&fullsize=" + $(".option-fullsize").is(":checked");
+
+        if( $(".option-already-changed").is(":checked") )
+        {
+          url += "&already-changed=" + $(".option-already-changed").is(":checked") +
+                 "&quantity="        + $(".already-changed-quantity").val();
+        };
+      return url;
+    };
 
     this.init = function(){
-      this.setEventsAndConst();
-      this.updateWidget();
-    }
+      self = this;
+      $(document).ready(function(){
+        self.setEventsAndConst();
+        self.updateWidget();
+      });
+    };
+
   };
 
   window.WidgetDemo = new WidgetDemo();
